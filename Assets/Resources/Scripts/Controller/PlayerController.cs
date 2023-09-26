f7ab966e1489b17c494921fdd0ebe3cb2114a1f1
Rokuel <kazix135@gmail.com>
2023-09-26 20:57:17 +0900

    09262057

--- Assets/Resources/Scripts/Controller/PlayerController.cs
+++ Assets/Resources/Scripts/Controller/PlayerController.cs
@@ -9,13 +9,12 @@ public class PlayerController : MonoBehaviour
     Stats stats = new Stats();
 
     int playerDir = 1;
-
+    [SerializeField]
+    float slopeCheckDistance;
     float maxSlopeAngle = 50f;
-    float slopeDownAngle;
-    float slopeSideAngle;
-    float lastSlopeAngle;
     float groundCheckRadius = 1f;
     float h;
+    float angle;
 
 
     [SerializeField]
@@ -26,9 +25,8 @@ public class PlayerController : MonoBehaviour
     bool isGrounded;
     bool isJumping;
     bool isOnSlope;
-    bool canWalkOnSlope;
-    bool canJump;
 
+    Vector2 slopePerp;
     Vector2 playerVec;
 
     Vector2 colliderSize;
@@ -46,6 +44,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        slopeCheckDistance = 1;
         // Input 매니저를 통해 어떠한 키가 눌리면 OnKeyboard 함수가 실행하도록 함
         // 혹시라도 두번 눌릴경우 취소하고 다시 하도록 함
         GameManager.Inputs.KeyAction -= OnKeyboard;
@@ -60,6 +59,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        SlopeCheck();
         CheckGround();
         MoveMent();
         Debug.Log(h);
@@ -67,17 +67,30 @@ public class PlayerController : MonoBehaviour
 
     void SlopeCheck()
     {
-        Vector2 checkPos = transform.position - new Vector3(0, colliderSize.y / 2);
+        //바로 밑으로 레이를 찍어 히트 지점에서의 법선벡터 구하기
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, slopeCheckDistance, groundLayer);
+
+        if (hit)
+        {
+            //노말벡터 값에서 반시계 방향으로 90도 돌린 벡터 값
+            slopePerp = Vector2.Perpendicular(hit.normal).normalized;
+            //법선 벡터에서 벡터 업에 하면 각이 나오겠죠?
+            angle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if(angle != 0)
+            {
+                isOnSlope = true;
+            }
+            else
+            {
+                isOnSlope = false;
+            }
+
+            Debug.DrawLine(hit.point, hit.point + hit.normal, Color.yellow);
+            Debug.DrawLine(hit.point, hit.point + slopePerp, Color.blue);
+        }
     }
 
-    void SlopeCheckHorizontal()
-    {
-
-    }
-
-    void SlopeCheckVertical() 
-    { 
-    }
 
     void CheckGround()
     {
@@ -88,19 +101,17 @@ public class PlayerController : MonoBehaviour
         {
             isJumping = false;
         }
-
-        if (isGrounded && !isJumping && slopeDownAngle <= maxSlopeAngle)
-        {
-            canJump = true;
-        }
     }
 
     void MoveMent()
     {
-        if (isGrounded && !isOnSlope && !isJumping) // 경사로 아닐 때
+        if (isGrounded && isOnSlope && !isJumping && angle < maxSlopeAngle) // 경사로 일때
         {
-            playerVec.Set(stats.Speed * h, 0.0f);
-            rb.velocity = playerVec;
+            rb.velocity = slopePerp * stats.Speed * h ; // -1f인 이유는 반시계 방향 벡터라 다시 원래로 돌린 것
+        }
+        else if(isGrounded && !isOnSlope && !isJumping) // 경사로 아닐 때
+        {
+            rb.velocity = new Vector2(h * stats.Speed, rb.velocity.y);
         }
     }
 
@@ -115,11 +126,17 @@ public class PlayerController : MonoBehaviour
     {
         h = Input.GetAxisRaw("Horizontal");
 
-        if(h == 1 && playerDir == -1)
+
+        if (h == 0)
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+        else
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        if (h == 1 && playerDir == -1)
         {
             Flip();
         }
-        else if( h == -1 && playerDir == 1)
+        else if (h == -1 && playerDir == 1)
         {
             Flip();
         }
